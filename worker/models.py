8f062e428935fce267423dcd4b8f9cce9fff5fd4
xdunit <xdunit@gmail.com>
2023-06-23 20:21:08 +0600

    workers comments added

--- worker/models.py
+++ worker/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Worker(models.Model):
+    user = models.OneToOneField(to=User, null=True, blank=False, on_delete=models.SET_NULL)
     name = models.CharField(max_length=255)
     specialization = models.CharField(max_length=255)
     expected_salary = models.SmallIntegerField(null=False)
@@ -11,3 +13,14 @@ class Worker(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    worker = models.ForeignKey(to=Worker, on_delete=models.CASCADE)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.author.username
+
